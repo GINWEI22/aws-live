2ff9bbc52ad517d31af3fb7749477b26f304b62a
GINWEI22 <110080167+GINWEI22@users.noreply.github.com>
2024-04-16 14:46:03 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-2.cxs2o6o2cm3x.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Aacs3273"
+customdb = "employee"
+custombucket = "yauginwei-employee"
+customregion = "us-east-1"
+
